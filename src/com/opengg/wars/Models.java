a72a48c00c734e0239bd1a125020f164100b2ec6
warren <rlaterson@gmail.com>
2019-04-06 23:06:26 -0400

    You know you can't help me.

--- src/com/opengg/wars/Models.java
+++ src/com/opengg/wars/Models.java
@@ -7,9 +7,11 @@ public class Models {
     public static Model stone;
     public static Model factory;
     public static Model house;
+    public static Model mine;
     public static void init(){
         factory = Resource.getModel("TheFactory");
         stone = Resource.getModel("Rock_6");
         house = Resource.getModel("cottage");
+        mine = Resource.getModel("DIESPOM_");
     }
 }
