55f0fe6e73c884147ede9bbdfdb3e20b57991c91
warren <rlaterson@gmail.com>
2019-04-07 09:54:17 -0400

    Barack Obama

--- src/com/opengg/wars/components/Building.java
+++ src/com/opengg/wars/components/Building.java
@@ -72,7 +72,7 @@ public class Building extends GameObject{
                 var barracks = new UnitProducer(side);
                 barracks.addUnit(Unit.UType.INFANTRY, Tuple.of(GameResource.STONE, 25), Tuple.of(GameResource.FOOD, 20));
                 barracks.addUnit(Unit.UType.CAVALRY, Tuple.of(GameResource.STEEL, 20), Tuple.of(GameResource.FOOD, 12));
-                barracks.addUnit(Unit.UType.ARCHER, Tuple.of(GameResource.STONE, 20), Tuple.of(GameResource.WOOD, 35));
+                barracks.addUnit(Unit.UType.ARCHER, Tuple.of(GameResource.STONE, 25), Tuple.of(GameResource.WOOD, 45));
                 barracks.attach(new ModelComponent(Models.barrack).setScaleOffset(0.02f).setRotationOffset(new Vector3f(90,0,0)));
                 return barracks;
             case TOWN:
